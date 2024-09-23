a34b2a9b1864c998f41e4a89a9e158f585ea7fb0
calvin hewett <hewecalv@byui.edu>
2024-09-23 13:27:17 -0600

    numbers

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        for (int i = 1;i <=10; i ++)
+        {
+            Console.WriteLine(i);
+        }
     }
+
 }
