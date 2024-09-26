010d04a24bf5915dd68c523a023992d318d6e2ec
calvin hewett <hewecalv@byui.edu>
2024-09-26 15:22:27 -0600

    here is number 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("welcome to number game");
+        Random RandomGenNumb =new Random();
+        int number = RandomGenNumb.Next(1,101);
+        int guess = -1;
+        while (guess != number)
+        {
+            Console.Write("what is the magic?");
+          guess= int.Parse(Console.ReadLine());
+
+           if (guess < number)
+           {
+            Console.WriteLine("higher; guess again");
+           }
+           else if (guess > number)
+           {
+            Console.WriteLine("lower; guess again");
+           }
+           else 
+           {
+            Console.WriteLine("you guessed the number");
+           }
+
+            
+            
+    
+        }
+
+
+
     }
 }
